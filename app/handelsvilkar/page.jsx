d0b69033485d215a0318fd0d2f6789dfd2f200e5
Eskil K. Hagen <eskilkaspersen@gmail.com>
2023-09-09 13:49:09 +0200

    small fix

--- app/handelsvilkar/page.jsx
+++ app/handelsvilkar/page.jsx
@@ -1,6 +1,6 @@
 export default function Handelsvilkar() {
     return (
-        <section className="max-w-7xl mx-auto bg-clrprimary text-clrdark h-screen px-4 py-32 flex flex-col gap-8">
+        <section className="max-w-7xl mx-auto bg-clrprimary text-clrdark px-4 py-32 flex flex-col gap-8">
             <h1 className="text-5xl text-clrwhite font-black">
                 Salgsvilkår for [Bandnavn] Merchandise
             </h1>
