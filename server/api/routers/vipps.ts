d0a7461f8344150dca66262c9ccf7a9b855cd9e2
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2024-01-07 15:35:59 +0100

    chore(vipps.ts): remove unused imports and variables chore(vipps.ts): remove unused code and comments chore(vipps.ts): add TODO comments for error handling and DB change chore(vipps.ts): add TODO comment for error handling

--- server/api/routers/vipps.ts
+++ server/api/routers/vipps.ts
@@ -1,27 +1,13 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { db } from "@/server/db";
-import { Client } from "vipps_mobilepay_sdk";
+import { vipps } from "@/vipps/vipps";
 import { z } from "zod";
 
 const clientId = process.env.VIPPS_CLIENT_ID!;
 const clientSecret = process.env.VIPPS_CLIENT_SECRET!;
 
-export const vipps = Client({
-  merchantSerialNumber: process.env.VIPPS_MERCHANT_SERIAL_NUMBER!,
-  subscriptionKey: process.env.VIPPS_SUBSCRIPTION_KEY_SECONDARY!,
-  useTestMode: true,
-  retryRequests: false,
-  systemName: "My cool e-commerce system",
-  systemVersion: "0.0.1",
-  pluginVersion: "0.0.1",
-  pluginName: "My cool plugin",
-});
-
-const token = await vipps.auth.getToken({
-  clientId,
-  clientSecret,
-  subscriptionKey: process.env.VIPPS_SUBSCRIPTION_KEY_SECONDARY!,
-});
+// TODO: Add error handling
+// TODO: Add DB Change on success
 
 export const vippsRouter = createTRPCRouter({
   vippsCreateSession: publicProcedure
@@ -46,7 +32,7 @@ export const vippsRouter = createTRPCRouter({
           paymentDescription: "One pair of socks.",
         },
       });
-      
+
       if (!checkout.ok) {
         console.error("😟 Error creating checkout session 😟");
         console.log(checkout.message);
